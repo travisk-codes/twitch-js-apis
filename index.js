afcd73eb4de301dd6a65d23e58aa81d7c8c7e08c
Travis Kohlbeck <me+git@travisk.info>
2020-03-09 03:55:27 -0400

    cleans up webhook

--- index.js
+++ index.js
@@ -25,23 +25,26 @@ isStreamLive('travisk_streams')
  * twitch-webhooks
  * https://d-fischer.github.io/twitch-webhooks/
  */
+
 async function getSubscription() {
-	return await listener.subscribeToStreamChanges(userId, async () => {
-		if (HelixStream) {
-			console.log('yay stream!')
-			console.log(HelixStream.userDisplayName, HelixStream.title)
-		} else {
-			console.log('no stream!')
-		}
+	const listener = await WebHookListener.create(twitchClient, {
+		hostName: 'https://travisk.info/twitch-webhook',
+		port: 8090,
+		reverseProxy: { port: 443, ssl: true },
 	})
+	listener.listen()
+
+	function onStreamChange(stream = HelixStream) {
+		if (stream) {
+			console.log('yay stream!')
+			console.log(stream.userDisplayName, stream.title)
+		} else {
+			console.log('no stream...')
+		}
+	}
+	return await listener.subscribeToStreamChanges(userId, onStreamChange)
 }
 
-async function getListener() {
-	const listener = await WebHookListener.create(twitchClient, { port: 8090 })
-	listener.listen()
-	return listener
-}
-const listener = await getListener()
 const subscription = getSubscription()
 
 process.on('SIGINT', () => {
@@ -49,33 +52,13 @@ process.on('SIGINT', () => {
 	process.exit(0)
 })
 
-function onStreamChange(stream = HelixStream) {
-	if (stream) {
-		console.log('yay stream!')
-		console.log(stream.userDisplayName, stream.title)
-	} else {
-		console.log('no stream...')
-	}
-}
-
-async function webhook() {
-	const listener = await WebHookListener.create(twitchClient, { port: 8090 })
-	listener.listen()
-	const subscription = await listener.subscribeToStreamChanges(
-		userId,
-		onStreamChange,
-	)
-}
-
-webhook()
-
 /**
  * twitch-pubsub-client
  * https://d-fischer.github.io/twitch-pubsub-client/
  */
 
-const pubSubClient = new PubSubClient()
-await pubSubClient.registerUserListener(twitchClient)
+//const pubSubClient = new PubSubClient()
+//await pubSubClient.registerUserListener(twitchClient)
 // waiting on response from discord channel for how to use
 // pub sub with chat messages and/or follows
 // https://discordapp.com/channels/325552783787032576/350012219003764748/686454266860142615
