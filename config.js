d6f00567bb5f08ec579341ee221a356fe4aeae25
Travis Kohlbeck <me+git@travisk.info>
2020-03-09 02:54:58 -0400

    adds apis, package.json, etc.

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	userId: '280687248',
+	clientId: 'd0ji7kwpkohwi5gfeye2y94en2djsv',
+	secret: 've9vreyfe0zj2lyv1uxjsj4o55g3mr',
+}
---
+++ index.js
@@ -0,0 +1,83 @@
+const TwitchClient = require('twitch').default
+const HelixStream = require('twitch').HelixStream
+const WebHookListener = require('twitch-webhooks').default
+const PubSubClient = require('twitch-pubsub-client').default
+const { PubSubChatMessage } = require('twitch-pubsub-client')
+
+const { userId, clientId, secret } = require('./config')
+
+const twitchClient = TwitchClient.withClientCredentials(clientId, secret)
+
+/**
+ * twitch (main)
+ * https://d-fischer.github.io/twitch/
+ */
+async function isStreamLive(username) {
+	const user = await twitchClient.helix.users.getUserByName(username)
+	if (!user) return false
+	const stream = await twitchClient.helix.streams.getStreamByUserId(user.id)
+	return stream !== null
+}
+
+isStreamLive('travisk_streams')
+
+/**
+ * twitch-webhooks
+ * https://d-fischer.github.io/twitch-webhooks/
+ */
+async function getSubscription() {
+	return await listener.subscribeToStreamChanges(userId, async () => {
+		if (HelixStream) {
+			console.log('yay stream!')
+			console.log(HelixStream.userDisplayName, HelixStream.title)
+		} else {
+			console.log('no stream!')
+		}
+	})
+}
+
+async function getListener() {
+	const listener = await WebHookListener.create(twitchClient, { port: 8090 })
+	listener.listen()
+	return listener
+}
+const listener = await getListener()
+const subscription = getSubscription()
+
+process.on('SIGINT', () => {
+	subscription.stop()
+	process.exit(0)
+})
+
+function onStreamChange(stream = HelixStream) {
+	if (stream) {
+		console.log('yay stream!')
+		console.log(stream.userDisplayName, stream.title)
+	} else {
+		console.log('no stream...')
+	}
+}
+
+async function webhook() {
+	const listener = await WebHookListener.create(twitchClient, { port: 8090 })
+	listener.listen()
+	const subscription = await listener.subscribeToStreamChanges(
+		userId,
+		onStreamChange,
+	)
+}
+
+webhook()
+
+/**
+ * twitch-pubsub-client
+ * https://d-fischer.github.io/twitch-pubsub-client/
+ */
+
+const pubSubClient = new PubSubClient()
+await pubSubClient.registerUserListener(twitchClient)
+// waiting on response from discord channel for how to use
+// pub sub with chat messages and/or follows
+// https://discordapp.com/channels/325552783787032576/350012219003764748/686454266860142615
+
+// message eiymba github repo
